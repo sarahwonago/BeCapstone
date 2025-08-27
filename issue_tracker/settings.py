a7f8b9a057c050781aee50fd6c61470ce5a24fe0
sarahwonago <sarahshaaleh@gmail.com>
2025-08-27 14:56:24 +0300

    increase the access token lifetime to days

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
 
 # JWT settings
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
