b3db2e9b21e0db06354e3abd27df85e704a8c5ec
sarahwonago <sarahshaaleh@gmail.com>
2025-08-28 14:01:22 +0300

    custom admin interface for easy management

--- apps/kb/admin.py
+++ apps/kb/admin.py
@@ -4,6 +4,16 @@ from .models import KnowledgeBaseArticle
 
 @admin.register(KnowledgeBaseArticle)
 class KnowledgeBaseArticleAdmin(admin.ModelAdmin):
-    list_display = ("title", "related_issue", "created_by", "created_at")
-    list_filter = ("created_at",)
+    list_display = ("title", "created_by", "related_issue", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
     search_fields = ("title", "content", "tags")
+    raw_id_fields = ("related_issue", "created_by")
+    readonly_fields = ("created_at", "updated_at")
+    fieldsets = (
+        (None, {"fields": ("title", "content", "tags")}),
+        ("Related Data", {"fields": ("related_issue", "created_by")}),
+        (
+            "Timestamps",
+            {"fields": ("created_at", "updated_at"), "classes": ("collapse",)},
+        ),
+    )
